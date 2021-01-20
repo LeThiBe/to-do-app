50ea4a135be7d4071eb130fe388da116d984c0c7
belt-1782 <le.thi.be@sun-asterisk.com>
2021-01-20 16:26:48 +0700

    change home page use class component

--- src/page/home/HomePage.jsx
+++ src/page/home/HomePage.jsx
@@ -1,14 +1,20 @@
+import React, {Component} from "react"
+
 import * as Style from './styles';
 
 import Header from '../../components/layout/header/Header';
 
-const HomePage = () => (
-  <>
-  <Header />
-  <Style.MainContainer>
-    <Style.TitleContent>This is home page</Style.TitleContent>
-  </Style.MainContainer>
-  </>
-);
+class HomePage extends Component {
+  render() {
+    return (
+      <>
+        <Header />
+        <Style.MainContainer>
+          <Style.TitleContent>This is home page</Style.TitleContent>
+        </Style.MainContainer>
+      </>
+    );
+  }
+}
 
 export default HomePage;
