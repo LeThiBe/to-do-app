dc64f2d479234df4414ee4deb9c395dfdb0ef05f
belt-1782 <le.thi.be@sun-asterisk.com>
2021-01-21 14:43:49 +0700

    increase and decrease value use props and state

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import {  BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import Home from './pages/Home';
@@ -12,7 +11,7 @@ function App() {
       <Router>
         <Switch>
           <Route exact path="/" component={Home} />
-          <Route path="/about" component={About} />
+          <Route path="/about" component={(props) => (<About number={1}  {...props}/>)} />)}
           <Route path="/contact" component={Contact} />
         </Switch>
       </Router>
--- src/pages/About/index.jsx
+++ src/pages/About/index.jsx
@@ -1,14 +1,45 @@
+import React, { Component } from 'react';
+
 import * as Style from './styles';
 
 import Header from '../../components/layout/header/Header';
 
-const About = () => (
-  <>
-  <Header />
-  <Style.MainContainer>
-    <Style.TitleContent>This is about page</Style.TitleContent>
-  </Style.MainContainer>
-  </>
-);
+class About extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      point: 0,
+    };
+
+    this.increase = this.increase.bind(this);
+    this.decrease = this.decrease.bind(this);
+  }
+
+  increase() {
+    this.setState((state, props) => ({
+      point: state.point + props.number
+    }));
+  }
+
+  decrease() {
+    this.setState((state, props) => ({
+      point: state.point - props.number
+    }));
+  }
+
+  render() {
+    return (
+      <>
+        <Header />
+        <Style.MainContainer>
+          <Style.TitleContent>This is about page</Style.TitleContent>
+          <Style.ButtonIncrease onClick={this.decrease}>-</Style.ButtonIncrease>
+          <Style.NumberPoint>Point is {this.state.point}</Style.NumberPoint>
+          <Style.ButtonIncrease onClick={this.increase}>+</Style.ButtonIncrease>
+        </Style.MainContainer>
+      </>
+    );
+  }
+}
 
 export default About;
