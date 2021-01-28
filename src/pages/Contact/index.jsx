fbdb6b76eeb1019a416242c81b0887feb11f49a4
belt-1782 <le.thi.be@sun-asterisk.com>
2021-01-28 12:59:12 +0700

    show number use useState and useEffect

--- src/pages/Contact/index.jsx
+++ src/pages/Contact/index.jsx
@@ -1,14 +1,34 @@
+import React, { useState, useEffect } from 'react';
+
 import * as Style from './styles';
 
 import Header from '../../components/layout/header/Header';
 
-const Contact = () => (
-  <>
-  <Header />
-  <Style.MainContainer>
-    <Style.TitleContent>This is contact page</Style.TitleContent>
-  </Style.MainContainer>
-  </>
-);
+const Contact = () => {
+  const twoDigit = (input) => {
+    var twodigit = input >= 10 ? input : "0"+ input.toString();
+
+    return twodigit;
+  }
+
+  const [number, setNumber] = useState(0);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setNumber(number + 1);
+    }, 1000)
+  }, [number]);
+
+  return (
+    <>
+      <Header />
+      <Style.MainContainer>
+        <Style.TitleContent>This is contact page</Style.TitleContent>
+        <Style.TitleNumber>Number is:</Style.TitleNumber>
+        <Style.ContentNumber>{twoDigit(number)}</Style.ContentNumber>
+      </Style.MainContainer>
+    </>
+  )
+};
 
 export default Contact;
--- src/pages/Contact/styles.js
+++ src/pages/Contact/styles.js
@@ -2,8 +2,17 @@ import styled from 'styled-components';
 
 export const MainContainer = styled.div`
   font-size: 10px;
+  text-align: center;
 `;
 
 export const TitleContent = styled.h1`
   font-size: 30px;
+`;
+
+export const TitleNumber = styled.label`
+  font-size: 30px;
+`;
+export const ContentNumber = styled.label`
+  font-size: 25px;
+  margin-left: 15px;
 `;
